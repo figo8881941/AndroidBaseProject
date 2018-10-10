2b416fb8a35685fb74151a9c633e4d00d1b31436
figo1874 <176511609@qq.com>
2018-10-10 11:31:17 +0800

    增加注释

--- app/src/main/java/com/duoduo/androidbaseproject/application/ProjectApplication.java
+++ app/src/main/java/com/duoduo/androidbaseproject/application/ProjectApplication.java
@@ -10,6 +10,7 @@ public class ProjectApplication extends MultiDexApplication {
     @Override
     public void onCreate() {
         super.onCreate();
+        //不同的进程，创建不同的代理类，初始化不同的内容
         BaseApplicationProxy proxy = createProxy(this);
         proxy.onCreate();
     }
@@ -21,10 +22,12 @@ public class ProjectApplication extends MultiDexApplication {
      * @return
      */
     public BaseApplicationProxy createProxy(Application application) {
+        //获取当前进程名称
         String processName = AppUtils.getCurProcessName(application);
         if (!TextUtils.isEmpty(processName)) {
             String pkgname = application.getPackageName();
             if (processName.equals(pkgname)) {
+                //如果是主进程
                 return new MainProcessApplicationProxy(application);
             }
         }
