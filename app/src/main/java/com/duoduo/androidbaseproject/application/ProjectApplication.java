4586273f38aec7b158bb450d49e560e50835ada7
figo1874 <176511609@qq.com>
2018-10-10 09:56:53 +0800

    增加自定义application

---
+++ app/src/main/java/com/duoduo/androidbaseproject/application/ProjectApplication.java
@@ -0,0 +1,10 @@
+package com.duoduo.androidbaseproject.application;
+
+import android.support.multidex.MultiDexApplication;
+
+public class ProjectApplication extends MultiDexApplication {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
