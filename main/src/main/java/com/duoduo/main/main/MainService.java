71cfab0e42cbe48a2e26e5cfe22bf69de00ac869
figo1874 <176511609@qq.com>
2018-10-10 11:40:00 +0800

    修改配置

---
+++ main/src/main/java/com/duoduo/main/main/MainService.java
@@ -0,0 +1,28 @@
+package com.duoduo.main.main;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+/**
+ * 主服务器
+ */
+public class MainService extends Service {
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+}
