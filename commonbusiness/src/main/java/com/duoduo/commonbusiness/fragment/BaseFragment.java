1280ff35b39679b4e78d75b771a41ba633617f76
王焯斌 <wangzhuobin@xmiles.cn>
2018-10-19 16:24:14 +0800

    格式化

--- commonbusiness/src/main/java/com/duoduo/commonbusiness/fragment/BaseFragment.java
+++ commonbusiness/src/main/java/com/duoduo/commonbusiness/fragment/BaseFragment.java
@@ -13,26 +13,32 @@ public abstract class BaseFragment extends Fragment {
     /**
      * 选中的回调
      */
-    public void onSelected() {}
+    public void onSelected() {
+    }
 
     /**
      * 失去选中的回掉
      */
-    public void onUnSelected() {}
+    public void onUnSelected() {
+    }
 
     /**
      * 页面滚动
+     *
      * @param position
      * @param positionOffset
      * @param positionOffsetPixels
      */
-    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels){}
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+    }
 
     /**
      * 滚动状态改变
+     *
      * @param state
      */
-    public void onPageScrollStateChanged(int state){}
+    public void onPageScrollStateChanged(int state) {
+    }
 
     public boolean isSelected() {
         return isSelected;
