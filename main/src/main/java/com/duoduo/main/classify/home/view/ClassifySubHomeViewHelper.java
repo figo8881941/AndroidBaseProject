663085d499d2a265974834e64a868ca1e0f4241a
王焯斌 <wangzhuobin@xmiles.cn>
2018-11-06 17:36:46 +0800

    修改banner样式

--- main/src/main/java/com/duoduo/main/classify/home/view/ClassifySubHomeViewHelper.java
+++ main/src/main/java/com/duoduo/main/classify/home/view/ClassifySubHomeViewHelper.java
@@ -3,7 +3,6 @@ package com.duoduo.main.classify.home.view;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
 import com.duoduo.main.R;
@@ -12,7 +11,6 @@ import com.duoduo.main.classify.home.data.ClassifySubHomeDataBean;
 import com.duoduo.main.common.image.BannerGlideImageLoader;
 import com.youth.banner.Banner;
 import com.youth.banner.BannerConfig;
-import com.youth.banner.Transformer;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -96,22 +94,18 @@ public class ClassifySubHomeViewHelper {
         }
 
         ArrayList<String> images = new ArrayList<String>();
-        ArrayList<String> titles = new ArrayList<String>();
         for (ClassifySubHomeDataBean.ModuleDtoListEntity.EntranceItemDtoListEntity entranceItemDtoListEntity : entranceItemDtoListEntities) {
             images.add(entranceItemDtoListEntity.getImg());
-            titles.add(entranceItemDtoListEntity.getTitle());
         }
 
         Banner banner = (Banner) LayoutInflater.from(context).inflate(R.layout.main_classify_sub_home_fragment_banner, parent, false);
         parent.addView(banner);
         //设置banner样式
-        banner.setBannerStyle(BannerConfig.CIRCLE_INDICATOR_TITLE);
+        banner.setBannerStyle(BannerConfig.CIRCLE_INDICATOR);
         //设置图片加载器
         banner.setImageLoader(new BannerGlideImageLoader());
         //设置图片集合
         banner.setImages(images);
-        //设置标题集合（当banner样式有显示title时）
-        banner.setBannerTitles(titles);
         //设置指示器位置（当banner模式中有指示器时）
         banner.setIndicatorGravity(BannerConfig.CENTER);
         //banner设置方法全部调用完毕时最后调用
