96c1490e86b4609f042f67bf5b558ac0b68d0e35
figo1874 <176511609@qq.com>
2018-11-15 17:17:09 +0800

    修改分类首页继承实现

--- main/src/main/java/com/duoduo/main/classify/home/view/ClassifySubHomeAdapter.java
+++ main/src/main/java/com/duoduo/main/classify/home/view/ClassifySubHomeAdapter.java
@@ -1,124 +1,28 @@
 package com.duoduo.main.classify.home.view;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
-import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
-import com.duoduo.main.R;
-
-import java.util.List;
+import com.duoduo.commonbase.adapter.QuickHeaderFooterRecyclerViewAdapter;
+import com.duoduo.main.classify.home.data.ClassifySubHomeDataBean;
 
 /**
  * 分类首页adapter
  */
-public class ClassifySubHomeAdapter extends RecyclerView.Adapter {
+public class ClassifySubHomeAdapter extends QuickHeaderFooterRecyclerViewAdapter<ClassifySubHomeDataBean.ModuleDtoListEntity.ProductInfoListEntity> {
 
-    private enum ITEM_TYPE {
-        HEADER_ITEM_TYPE,
-        FOOTER_ITEM_TYPE,
-        LIST_ITEM_TYPE
-    }
-
-    private Context context;
-
-    private View headerView;
-
-    private View footerView;
-
-    private LayoutInflater layoutInflater;
-
-    private List<Object> data;
 
     public ClassifySubHomeAdapter(Context context) {
-        super();
-        this.context = context.getApplicationContext();
-        layoutInflater = LayoutInflater.from(this.context);
+        super(context);
     }
 
     @Override
-    public int getItemViewType(int position) {
-        if (headerView != null && position == 0) {
-            return ITEM_TYPE.HEADER_ITEM_TYPE.ordinal();
-        } else if (footerView != null && position == getItemCount() - 1) {
-            return ITEM_TYPE.FOOTER_ITEM_TYPE.ordinal();
-        }
-        return ITEM_TYPE.LIST_ITEM_TYPE.ordinal();
-    }
-
-    @NonNull
-    @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        if (viewType == ITEM_TYPE.HEADER_ITEM_TYPE.ordinal()) {
-            return new RecyclerView.ViewHolder(headerView) {
-            };
-        } else if (viewType == ITEM_TYPE.FOOTER_ITEM_TYPE.ordinal()) {
-            return new RecyclerView.ViewHolder(footerView) {
-            };
-        } else {
-            View itemView = layoutInflater.inflate(R.layout.main_common_two_list_item_layout, parent, false);
-            return new ViewHolder(itemView);
-        }
+    public int getViewLayoutId(int viewType) {
+        return 0;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-        int viewType = getItemViewType(position);
-        if (viewType == ITEM_TYPE.HEADER_ITEM_TYPE.ordinal()) {
-            return;
-        } else if (viewType == ITEM_TYPE.FOOTER_ITEM_TYPE.ordinal()) {
-            return;
-        } else {
+    public void bindViewHolder(QuickerViewHolder holder, ClassifySubHomeDataBean.ModuleDtoListEntity.ProductInfoListEntity itemData, int position) {
 
-        }
-    }
-
-    @Override
-    public int getItemCount() {
-        int count = 0;
-        if (headerView != null) {
-            count++;
-        }
-        if (footerView != null) {
-            count++;
-        }
-        if (data != null) {
-            count += data.size();
-        }
-        return count;
-    }
-
-    public View getHeaderView() {
-        return headerView;
-    }
-
-    public void setHeaderView(View headerView) {
-        this.headerView = headerView;
-    }
-
-    public View getFooterView() {
-        return footerView;
-    }
-
-    public void setFooterView(View footerView) {
-        this.footerView = footerView;
-    }
-
-    public List<Object> getData() {
-        return data;
-    }
-
-    public void setData(List<Object> data) {
-        this.data = data;
-    }
-
-    public static class ViewHolder extends RecyclerView.ViewHolder {
-
-        public ViewHolder(View itemView) {
-            super(itemView);
-        }
     }
 
 }
