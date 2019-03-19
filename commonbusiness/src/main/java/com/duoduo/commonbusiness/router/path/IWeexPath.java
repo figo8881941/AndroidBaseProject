1a5e3c92b0a2ee1fdaef336f8270ace73d58d77f
figo1874 <176511609@qq.com>
2019-03-19 16:04:16 +0800

    增加Weex容器

---
+++ commonbusiness/src/main/java/com/duoduo/commonbusiness/router/path/IWeexPath.java
@@ -0,0 +1,12 @@
+package com.duoduo.commonbusiness.router.path;
+
+/**
+ * Weex模块路由路径常量
+ */
+public interface IWeexPath {
+
+    /**
+     * 通用Weex页面容器路径
+     */
+    public String COMMON_WEEX_ACTIVITY = "/weex/container/CommonWeexContainer";
+}
---
+++ weex/src/main/java/com/duoduo/weex/container/CommonWeexActivity.java
@@ -0,0 +1,13 @@
+package com.duoduo.weex.container;
+
+import com.alibaba.android.arouter.facade.annotation.Route;
+import com.duoduo.commonbusiness.activity.BaseLoadingDialogActivity;
+import com.duoduo.commonbusiness.router.path.IWeexPath;
+
+/**
+ * Weex容器Activity
+ */
+
+@Route(path = IWeexPath.COMMON_WEEX_ACTIVITY)
+public class CommonWeexActivity extends BaseLoadingDialogActivity {
+}
