1cbe5d0d1e8cf23135246111501a0a515186b39c
figo1874 <176511609@qq.com>
2019-04-20 18:38:21 +0800

    去除无用代码

--- main/src/main/java/com/duoduo/main/main/model/MainModel.java
+++ main/src/main/java/com/duoduo/main/main/model/MainModel.java
@@ -11,12 +11,6 @@ import com.duoduo.main.main.event.MainTabRequestEvent;
 import org.greenrobot.eventbus.EventBus;
 import org.json.JSONObject;
 
-import java.io.IOException;
-
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.Response;
-
 /**
  * 主界面Model
  */
@@ -28,45 +22,6 @@ public class MainModel extends BaseModel implements IMainModel {
         super(context);
         mainNetModel = new MainNetModel(context);
     }
-
-//    /**
-//     * 请求首页Tab数据的方法
-//     */
-//    public void requestTabData() {
-//        final EventBus eventBus = EventBus.getDefault();
-//        final MainTabRequestEvent event = new MainTabRequestEvent();
-//
-//        //通知开始
-//        event.setWhat(MainTabRequestEvent.EVENT_NAME_REQUEST_START);
-//        eventBus.post(event);
-//
-//        try {
-//            mainNetModel.requestTabData(new Response.Listener<JSONObject>() {
-//                @Override
-//                public void onResponse(JSONObject response) {
-//                    //通知请求完成
-//                    event.setWhat(MainTabRequestEvent.EVENT_NAME_REQUEST_SUCCESS);
-//                    MainTabEntity mainTabEntity = JSON.parseObject(response.toString(), MainTabEntity.class);
-//                    event.setArg3(mainTabEntity);
-//                    eventBus.post(event);
-//                }
-//            }, new Response.ErrorListener() {
-//                @Override
-//                public void onErrorResponse(VolleyError error) {
-//                    //通知出错
-//                    event.setWhat(MainTabRequestEvent.EVENT_NAME_REQUEST_ERROR);
-//                    event.setArg4(error);
-//                    eventBus.post(event);
-//                }
-//            });
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//            //通知出错
-//            event.setWhat(MainTabRequestEvent.EVENT_NAME_REQUEST_ERROR);
-//            event.setArg4(e);
-//            eventBus.post(event);
-//        }
-//    }
 
     /**
      * 请求首页Tab数据的方法
